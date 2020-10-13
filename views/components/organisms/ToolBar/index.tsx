957f733f64c8805bd94890bb99cbefa01a245386
TsurutaShingo <gmT.S8.531@gmail.com>
2020-10-13 23:58:00 +0900

    [add] function toToolBar

--- views/components/organisms/ToolBar/index.tsx
+++ views/components/organisms/ToolBar/index.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { play, stop } from '../../../../redux/audio/operations';
+import { getIsPlaying } from '../../../../redux/audio/selectors';
 import { Container } from './Container';
 import { ToolItems } from './ToolItems';
 
 export const ToolBar: React.FC = () => {
+  const dispatch = useDispatch();
+  const selector = useSelector(state => state);
+  const isPlaying = getIsPlaying(selector);
+
   return (
     <Container>
-      <ToolItems.Play onClick={() => {}} />
+      {isPlaying ?
+        <ToolItems.Stop onClick={() => dispatch(stop())} /> :
+        <ToolItems.Play onClick={() => dispatch(play())} />
+      }
     </Container>
   );
 };
