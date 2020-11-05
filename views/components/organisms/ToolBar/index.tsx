dd5582c7df2662b4d36c289d6de4a2cab402d043
TsurutaShingo <gmT.S8.531@gmail.com>
2020-11-05 19:24:44 +0900

    [add] BpmInputComponent on ToolBarComponent

--- views/components/organisms/ToolBar/index.tsx
+++ views/components/organisms/ToolBar/index.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { play, stop } from '../../../../redux/audio/operations';
 import { getIsPlaying } from '../../../../redux/audio/selectors';
 import { Container } from './Container';
+import { BpmInput } from './BpmInput';
 import { ToolItems } from './ToolItems';
 
 export const ToolBar: React.FC = () => {
@@ -12,6 +13,7 @@ export const ToolBar: React.FC = () => {
 
   return (
     <Container>
+      <BpmInput />
       {isPlaying ?
         <ToolItems.Stop onClick={() => dispatch(stop())} /> :
         <ToolItems.Play onClick={() => dispatch(play())} />
