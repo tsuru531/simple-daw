8131e90dbae6ac8d028fb84ffd1051cb55c6a936
TsurutaShingo <gmT.S8.531@gmail.com>
2020-11-06 19:31:43 +0900

    [create] useMasterVolKnob

---
+++ models/hooks/useMasterVolKnob.ts
@@ -0,0 +1,13 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { getMasterVol, setMasterVol } from '../../redux/audio';
+import { useKnob } from './useKnob';
+
+export const useMasterVolKnob = () => {
+  const dispatch = useDispatch();
+  const selecter = useSelector(state => state);
+  const volume: number = getMasterVol(selecter);
+  const step: number = 0.01;
+  const ref = useKnob(volume, (value) => {dispatch(setMasterVol(value))}, step);
+
+  return ref;
+};
