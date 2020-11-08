541128c6e7b98ad7cff3a2fe4dba341b9b845775
TsurutaShingo <gmT.S8.531@gmail.com>
2020-11-08 19:51:31 +0900

    [add] notesState

--- redux/initialState.ts
+++ redux/initialState.ts
@@ -4,6 +4,20 @@ export const initialState = {
     masterVol: 1,
     masterLevel: 0,
     bpm: 120,
+    notes: [
+      {keyNum: 76, startTime: 0, length: .5},
+      {keyNum: 72, startTime: .5, length: .5},
+      {keyNum: 67, startTime: 1, length: .5},
+      {keyNum: 72, startTime: 1.5, length: .5},
+      {keyNum: 74, startTime: 2, length: .5},
+      {keyNum: 79, startTime: 2.5, length: 1},
+      {keyNum: 67, startTime: 3.5, length: .5},
+      {keyNum: 74, startTime: 4, length: .5},
+      {keyNum: 76, startTime: 4.5, length: .5},
+      {keyNum: 74, startTime: 5, length: .5},
+      {keyNum: 67, startTime: 5.5, length: .5},
+      {keyNum: 72, startTime: 6, length: 1}
+    ],
     track: [
       {
         vol: .1,
