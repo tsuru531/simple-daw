6accf2ee611db57e0816268c60334bd5ecab89e1
TsurutaShingo <gmT.S8.531@gmail.com>
2020-11-08 19:52:28 +0900

    [delete] trackState

--- redux/initialState.ts
+++ redux/initialState.ts
@@ -18,30 +18,5 @@ export const initialState = {
       {keyNum: 67, startTime: 5.5, length: .5},
       {keyNum: 72, startTime: 6, length: 1}
     ],
-    track: [
-      {
-        vol: .1,
-        region: [
-          {
-            startTime: 0,
-            length: 0,
-            note: [
-              {keyNum: 76, startTime: 0, length: .5},
-              {keyNum: 72, startTime: .5, length: .5},
-              {keyNum: 67, startTime: 1, length: .5},
-              {keyNum: 72, startTime: 1.5, length: .5},
-              {keyNum: 74, startTime: 2, length: .5},
-              {keyNum: 79, startTime: 2.5, length: 1},
-              {keyNum: 67, startTime: 3.5, length: .5},
-              {keyNum: 74, startTime: 4, length: .5},
-              {keyNum: 76, startTime: 4.5, length: .5},
-              {keyNum: 74, startTime: 5, length: .5},
-              {keyNum: 67, startTime: 5.5, length: .5},
-              {keyNum: 72, startTime: 6, length: 1}
-            ]
-          }
-        ]
-      }
-    ]
   }
 };
