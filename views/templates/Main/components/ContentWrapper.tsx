e379c78e87ccaed7e680b2ad71f32a7b5209c985
TsurutaShingo <gmT.S8.531@gmail.com>
2020-11-11 20:28:09 +0900

    [styled] ContentWrapperComponent

--- views/templates/Main/components/ContentWrapper.tsx
+++ views/templates/Main/components/ContentWrapper.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const ContentWrapper = styled.div`
   height: 100%;
   width: 100%;
+  padding: 8px;
   background-color: #515151;
   border-radius: 8px;
 `;
