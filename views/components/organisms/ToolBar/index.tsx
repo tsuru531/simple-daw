654d9f5f6e583347656ea94afe9f543d1936cce5
TsurutaShingo <gmT.S8.531@gmail.com>
2020-11-13 16:59:50 +0900

    [clean] ToolBarComponent

--- views/components/organisms/ToolBar/index.tsx
+++ views/components/organisms/ToolBar/index.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { play, stop } from '../../../../redux/audio/operations';
-import { getIsPlaying } from '../../../../redux/audio/selectors';
+import { play, stop, getIsPlaying, Types } from '../../../../redux/audio';
 import { Container } from './Container';
 import { BpmInput } from './BpmInput';
 import { ToolItems } from './ToolItems';
-import { Types } from 'src/redux/audio';
 
 export const ToolBar: React.FC = () => {
   const dispatch = useDispatch();
