100e890cc97303a5968a61eda3b65c76fbd6fc64
TsurutaShingo <gmT.S8.531@gmail.com>
2020-11-18 19:46:37 +0900

    [create] useTrackVolKnob

---
+++ models/hooks/useTrackVolKnob.ts
@@ -0,0 +1,14 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { getTrackVol, setTrackVol, Types } from '../../redux/audio';
+import { useKnob } from './useKnob';
+
+export const useTrackVolKnob = (id: string) => {
+  const dispatch = useDispatch();
+  const selector = useSelector((state: Types.state) => state);
+  const volume: number = getTrackVol(selector, id);
+  const step: number = 0.02;
+
+  const ref = useKnob(volume, (vol: number) => dispatch(setTrackVol(id, vol)) , step);
+
+  return ref;
+};
