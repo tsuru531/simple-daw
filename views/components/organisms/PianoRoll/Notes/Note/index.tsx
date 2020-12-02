de6b9bf04f2ab0178ed618948dd174e4cf2a8762
TsurutaShingo <gmT.S8.531@gmail.com>
2020-12-02 19:17:26 +0900

    [clean] NotesComponent

--- views/components/organisms/PianoRoll/Notes/Note/index.tsx
+++ views/components/organisms/PianoRoll/Notes/Note/index.tsx
@@ -9,10 +9,12 @@ type props = {
 
 export const Note: React.FC<props> = ({ note, noteSize }) => {
   const rollHeight: number = noteSize * 127;
+  const x: number = noteSize * note.startTime;
+  const y: number = rollHeight - noteSize * note.keyNum;
   const d: string = `M 0 0 L 0 ${noteSize} L ${noteSize * note.length} ${noteSize} L ${noteSize * note.length} 0`;
 
   return (
-    <g transform={`matrix(1, 0, 0, 1, ${noteSize * note.startTime},${rollHeight - noteSize * note.keyNum})`}>
+    <g transform={`matrix(1, 0, 0, 1, ${x}, ${y})`}>
       <Paint d={d} />
     </g>
   );
