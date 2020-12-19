79fd73e9ae994a51cab62ce35aa68f5de8e9864e
TsurutaShingo <gmT.S8.531@gmail.com>
2020-12-19 13:19:44 +0900

    create: RightComponent in NoteComponent

--- views/components/organisms/PianoRoll/Notes/Note/index.tsx
+++ views/components/organisms/PianoRoll/Notes/Note/index.tsx
@@ -1,25 +1,37 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { getNoteSize, Types } from '../../../../../../redux/audio';
-import { useNote } from '../../../../../../models/hooks/useNote';
+import { useNote } from '../../../../../../models/hooks';
 import { Body } from './Body';
+import { Right } from './Right';
 
 type props = {
   note: Types.note
 };
 
+type d = {
+  body: string,
+  right: string,
+  left: string
+};
+
 export const Note: React.FC<props> = ({ note }) => {
   const selector: Types.state = useSelector((state: Types.state) => state);
   const noteSize: number = getNoteSize(selector);
+  const noteRefs = useNote(note);
   const rollHeight: number = noteSize * 127;
   const x: number = noteSize * note.startTime;
   const y: number = rollHeight - noteSize * note.keyNum;
-  const d: string = `M 0 0 L 0 ${noteSize} L ${noteSize * note.length} ${noteSize} L ${noteSize * note.length} 0`;
-  const noteRefs = useNote(note);
+  const d: d = {
+    body: `M 0 0 L 0 ${noteSize} L ${noteSize * note.length} ${noteSize} L ${noteSize * note.length} 0`,
+    right: `M ${noteSize * note.length} 0 L ${noteSize * note.length} ${noteSize} L ${(noteSize * note.length) - 4} ${noteSize} L ${(noteSize * note.length) - 4} 0`,
+    left: ""
+  };
 
   return (
     <g transform={`matrix(1, 0, 0, 1, ${x}, ${y})`}>
-      <Body d={d} refObject={noteRefs.body} />
+      <Body d={d.body} refObject={noteRefs.body} />
+      <Right d={d.right} refObject={noteRefs.right} />
     </g>
   );
 };
