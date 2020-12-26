6e00c41c32025721431b12827a990188f648da3e
TsurutaShingo <gmT.S8.531@gmail.com>
2020-12-26 17:36:58 +0900

    delete: NoteInputComponent

--- views/templates/Main/components/Tracks/Track/index.tsx
+++ views/templates/Main/components/Tracks/Track/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { TrackWrapper} from './TrackWrapper';
 import { TypeInput } from './TypeInput';
 import { TrackVolKnob } from './TrackVolKnob';
-import { NoteInput } from './NoteInput';
 import { DeleteButton } from './DeleteButton';
 
 type props = {
@@ -15,7 +14,6 @@ export const Track: React.FC<props> = ({ trackId }) => {
       id: {trackId}
       <TypeInput id={trackId} />
       <TrackVolKnob trackId={trackId} />
-      <NoteInput trackId={trackId} />
       <DeleteButton id={trackId} />
     </TrackWrapper>
   );
