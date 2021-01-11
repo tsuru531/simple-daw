786d411e90cf04f83ffa8efc08bb029dcc1008bc
TsurutaShingo <gmT.S8.531@gmail.com>
2021-01-11 16:59:27 +0900

    update: TracksComponent

--- views/templates/Main/components/Tracks/index.tsx
+++ views/templates/Main/components/Tracks/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addTrack, getTracks, Types } from '../../../../../redux/audio';
+import { useSelector } from 'react-redux';
+import { getTracks, Types } from '../../../../../redux/audio';
 import { Track } from './Track';
 import { TracksWrapper } from './TracksWrapper';
 
+import { AddTrackButton } from '../../../../components/molecules';
+
 export const Tracks: React.FC = () => {
-  const dispatch = useDispatch();
   const selector = useSelector(state => state);
   const tracks = getTracks(selector as Types.state);
 
@@ -21,7 +22,7 @@ export const Tracks: React.FC = () => {
           )
         })}
       </ul>
-      <button onClick={() => dispatch(addTrack())}>addTrack</button>
+      <AddTrackButton />
     </TracksWrapper>
   );
 };
