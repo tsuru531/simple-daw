c5eadd98865c229f3720f7e8295afe0014f2b265
TsurutaShingo <gmT.S8.531@gmail.com>
2021-01-11 16:37:20 +0900

    add: buttonStyle to GlobalStyle

--- views/GlobalStyle.tsx
+++ views/GlobalStyle.tsx
@@ -19,4 +19,12 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+
+  button {
+    padding: 0;
+    background: none;
+    border: none;
+    outline: none;
+    appearance: none;
+  }
 `;
