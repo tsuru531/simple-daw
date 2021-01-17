d2c0a9bc4ae994815907f90f0c981b746456d0da
TsurutaShingo <gmT.S8.531@gmail.com>
2021-01-17 19:51:23 +0900

    create: components/atoms/ButtonRed

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -4,3 +4,4 @@ export { MainWrapper } from './MainWrapper';
 export { GridContainer } from './GridContainer';
 export { ToolContainer } from './ToolContainer';
 export { Padding8px } from './Padding8px';
+export { ButtonRed } from './ButtonRed';
