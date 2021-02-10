1c52e2e5311616e2131e10a94b5683e6e4db3a22
TsurutaShingo <gmT.S8.531@gmail.com>
2021-02-10 17:16:43 +0900

    update: components / app / atoms / container / ContentContainer

--- components/app/atoms/container/ContentContainer.tsx
+++ components/app/atoms/container/ContentContainer.tsx
@@ -1,9 +1,26 @@
+import * as React from 'react';
 import styled from 'styled-components';
 
-export const ContentContainer = styled.div`
-  height: 100%;
+export const ContentContainer: React.FC = React.memo(({ children }) => {
+  return (
+    <Container>
+      <Inner>
+        {children}
+      </Inner>
+    </Container>
+  );
+});
+
+const Container = styled.div`
   width: 100%;
+  height: 100%;
   padding: 8px;
   background-color: #515151;
   border-radius: 8px;
+`;
+
+const Inner = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: scroll;
 `;
