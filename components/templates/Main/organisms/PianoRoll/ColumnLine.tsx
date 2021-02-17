89b528891b0313a9564f3af6c0b10f5fa636a700
TsurutaShingo <gmT.S8.531@gmail.com>
2021-02-17 19:40:42 +0900

    update: ColumnLine component

--- components/templates/Main/organisms/PianoRoll/ColumnLine.tsx
+++ components/templates/Main/organisms/PianoRoll/ColumnLine.tsx
@@ -28,9 +28,8 @@ const FlexItem = styled.div`
 
   & + &:before {
     content: '';
-    border-left: solid 1px black;
+    border-left: solid 1px rgba(0, 0, 0, .2);
     position: absolute;
     height: 100%;
-    margin-left: -1px;
   }
 `;
