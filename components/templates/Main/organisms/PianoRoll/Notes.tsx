0243552f793eda79824ba09a0fb3b773c5d053c7
TsurutaShingo <gmT.S8.531@gmail.com>
2021-02-17 19:44:04 +0900

    update: Notes component

--- components/templates/Main/organisms/PianoRoll/Notes.tsx
+++ components/templates/Main/organisms/PianoRoll/Notes.tsx
@@ -1,19 +1,25 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 import { getNotesForSelectedTrack, Types } from '../../../../../redux/audio';
 import { Note } from './Note';
 
-export const Notes: React.FC = () => {
-  const selecter = useSelector((state: Types.state) => state);
-  const notes: Types.note[] = getNotesForSelectedTrack(selecter);
+export const Notes: React.FC = React.memo(() => {
+  const selector = useSelector((state: Types.state) => state);
+  const notes: Types.note[] = getNotesForSelectedTrack(selector);
 
   return (
-    <g>
+    <Container>
       {notes.map(note => {
         return (
           <Note key={note.id} note={note} />
-        )
+        );
       })}
-    </g>
+    </Container>
   );
-};
+});
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
