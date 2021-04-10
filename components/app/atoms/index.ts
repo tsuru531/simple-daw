21a61ffb5efad28cd9aea3650bb882a004d85945
Shingo Tsuruta <gmT.S8.531@gmail.com>
2021-04-10 19:39:09 +0900

    update: components/ app/ molecules/ button/ DeleteButton.tsx

--- components/app/atoms/index.ts
+++ components/app/atoms/index.ts
@@ -1,5 +1,3 @@
-export { RedButton } from './button/RedButton';
 export { ContentContainer } from './container/ContentContainer';
 export { MainContainer } from './container/MainContainer';
 export { AddIcon } from './icon/AddIcon';
-export { BucketIcon } from './icon/BucketIcon';
--- components/app/molecules/button/DeleteButton.tsx
+++ components/app/molecules/button/DeleteButton.tsx
@@ -1,14 +1,33 @@
 import * as React from 'react';
-import { RedButton, BucketIcon } from '../../atoms';
+import styled from 'styled-components';
 
 type props = {
-  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 };
 
 export const DeleteButton: React.FC<props> = React.memo(({onClick}) => {
   return (
-    <RedButton onClick={onClick}>
-      <BucketIcon />
-    </RedButton>
+    <Button onClick={onClick}>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#333"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>
+      </div>
+    </Button>
   );
 });
+
+const Button = styled.button`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+
+  &:hover {
+    background-color: rgba(255, 255, 255, .2);
+    cursor: pointer;
+  }
+  
+  & > div {
+    width: 24px;
+    height: 24px;
+    margin: 0 auto;
+  }
+`;
